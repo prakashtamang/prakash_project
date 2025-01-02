586481052485e6e025f0c7fd552e81e9507ca550
Prakash Tamang <meprakashtamang@gmail.com>
2025-01-03 00:41:51 +0545

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+(function(){
+    function toggleScrolled(){
+        const selectBody = document.querySelector('body');
+        const selectHeader = document.querySelector('#header');
+        window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    }
+    document.addEventListener('scroll', toggleScrolled);
+    window.addEventListener('load', toggleScrolled);
+
+    // Mobile Nav Toggle
+    document.querySelectorAll('.menu-toggle img').forEach((toggle) => {
+        toggle.addEventListener('click', function(e){
+            e.preventDefault();
+            document.querySelector('body').classList.toggle('mobile-nav-active');
+            e.stopImmediatePropagation();
+        })
+    });
+
+    // Initialize Swiper 
+
+    const swiper = new Swiper('.swiper', {
+        // autoplay: {
+        //     delay: 5000,
+        // },
+
+      
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+       
+
+        // Default parameters
+        slidesPerView: "auto",
+        spaceBetween: 10,
+        // Responsive breakpoints
+        breakpoints: {
+            768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+            },
+            992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+            },
+        }
+    });
+})();
